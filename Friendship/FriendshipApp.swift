a63062f7aafe897848153ec0e1236f319abc24c4
Jonas Agnezi <jonas.agnezi@gmail.com>
2022-11-06 18:13:01 -0300

    Initial Commit

---
+++ Friendship/FriendshipApp.swift
@@ -0,0 +1,18 @@
+//
+//
+// CupcakeCorner
+// Created by: itsjagnezi on 21/10/22
+// Copyright (c) today and beyond
+//
+
+
+import SwiftUI
+
+@main
+struct FriendshipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
